91bd7a0417b663a86049e017dffe403c95e2cae1
siddharth <notcodesid@gmail.com>
2024-12-29 13:06:54 -0500

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,15 +13,15 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: " Secreen Time Tracker",
-  description: "Tracks screen time for each tab. ",
+  title: "Screen Time Tracker",
+  description: "Tracks screen time for each tab.",
 
   // Open Graph metadata
   openGraph: {
-    title: " Screen Time Tracker",
-    description: "Tracks screen time for each tab. ",
+    title: "Screen Time Tracker",
+    description: "Tracks screen time for each tab.",
     url: "https://screen-time-blond.vercel.app/",
-    siteName: "Secreen Time Tracker",
+    siteName: "Screen Time Tracker",
     images: [
       {
         url: "/open-graph.png",
@@ -36,8 +36,8 @@ export const metadata: Metadata = {
 
   twitter: {
     card: "summary_large_image",
-    title: " Screen Time Tracker",
-    description: "Tracks screen time for each tab. ",
+    title: "Screen Time Tracker",
+    description: "Tracks screen time for each tab.",
     images: ["/open-graph.png"],
   },
 };
